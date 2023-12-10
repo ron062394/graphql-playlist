2b50c7bd495bbb260434aba7cf1ab373a6411828
Ron <ron062394@gmail.com>
2023-12-10 18:40:25 +0800

    Added schema dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,14 @@ const graphql = require('graphql');
 
 const {GrapQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
+//Dummy data
+var books = [
+  {name: 'Voice of the Wind', genre: 'Fantasy', id: '1' },
+  {name: 'Eyes of the Earth', genre: 'Fantasy', id: '2' },
+  {name: 'Ears of the Water', genre: 'Fantasy', id: '3' },
+  {name: 'Breath of the fire', genre: 'Fantasy', id: '4' },   
+  ]
+
 const BookType = new GrapQLObjectType({
     name: 'Book',
     fields: () => ({

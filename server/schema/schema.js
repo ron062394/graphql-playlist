d4a1f0996eb2af96c84c020bd964a2ad648585d2
Ron <ron062394@gmail.com>
2023-12-10 18:28:31 +0800

    Updated schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,31 @@
 const graphql = require('graphql');
 
-const {GrapqlObjectType, GraphQLString} = graphql;
+const {GrapQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
-const BookType = new GrapqlObjectType({
+const BookType = new GrapQLObjectType({
     name: 'Book',
     fields: () => ({
         id: {type: GraphQLString},
         name: {type: GraphQLString},
         genre: {type: GraphQLString},
     })
+})
+
+
+const RootQuery = new GrapQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType, 
+      args: {id: {type: GraphQLString}}, 
+      resolve(parent, args) {
+        args.id
+      }
+    }
+  }
+})
+
+
+module.exports = new GraphQLSchema({
+  RootQuery
 })
